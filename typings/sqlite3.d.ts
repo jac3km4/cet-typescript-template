06fe5994ee1e116299af4b9fb056f3a58d1775dc
Jelle Bakker <jellebakker15@live.nl>
2021-01-24 21:09:28 +0100

    added basics for sqlite3

--- typings/sqlite3.d.ts
+++ typings/sqlite3.d.ts
@@ -2,9 +2,21 @@ declare const sqlite3: sqlite3;
 
 /** @noSelf **/
 declare interface sqlite3 {
-    open(name: string): DB
+    complete(sql: string): boolean
+    open(filename: string, flags?: string): DB
+    version(): string
+    lversion(): string
 }
 
 declare interface DB {
     close(): void
+    rows(sql: string): Rows
+    urows(sql: string): Rows
+    changes(): number
+    exec(sql: string): void
+    isopen(): boolean
+    total_changes(): number
 }
+
+/** @luaIterator */
+type Rows = Iterable<string>;
